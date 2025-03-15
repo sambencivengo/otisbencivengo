b40b94477c1301dd5c664798124a729e7b430767
Sam Bencivengo <sambencivengo@gmail.com>
2025-03-15 16:48:23 -0400

    small adjustments to teaser

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p>
-
-          Stay tuned ❤️
-        </p>
-        <p>
-          - R & S
-        </p>
+        <div className="flex flex-col w-50 h-50 bg-red-800 justify-center items-center italic font-semibold text-xl">
+          <p >
+            Stay tuned ❤️ </p>
+          <p>
+            - R & S
+          </p>
+        </div>
       </main>
     </div>
   );
