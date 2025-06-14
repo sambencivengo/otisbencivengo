c32a8db16d085ceb3ba5fdd9f3c2a7b4a5f7f6b6
Sam Bencivengo <sambencivengo@gmail.com>
2025-06-14 16:25:58 -0400

    selective red hover

--- app/components/weddingTitle.tsx
+++ app/components/weddingTitle.tsx
@@ -1,8 +1,8 @@
 
 export default function WeddingTitle() {
-  return (<div className="w-full hover:text-red-700">
-    <p className="text-7xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">Bekah & Sam</p>
-    <p className="pt-2 text-3xl sm:text-3xl md:text-4xl tracking-[0.3em] sm:tracking-[0.4em] md:tracking-[0.5em]">10 | 10 | 2025</p>
-    <p className="text-3xl sm:text-3xl md:text-3xl pt-4">New York, New York</p>
+  return (<div className="w-full ">
+    <p className="text-7xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl hover:text-red-700 ">Bekah & Sam</p>
+    <p className="pt-2 text-3xl sm:text-3xl md:text-4xl tracking-[0.3em] sm:tracking-[0.4em] md:tracking-[0.5em] hover:text-red-700">10 | 10 | 2025</p>
+    <p className="text-3xl sm:text-3xl md:text-3xl pt-4 hover:text-red-700">New York, New York</p>
   </div>)
 }
