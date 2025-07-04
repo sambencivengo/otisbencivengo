105df78915f1fa6a3280149f138eb959e266f9e2
Sam Bencivengo <sambencivengo@gmail.com>
2025-07-04 10:22:35 -0400

    red spinner

--- app/components/spinner.tsx
+++ app/components/spinner.tsx
@@ -3,5 +3,5 @@ import { Loader2 } from 'lucide-react';
 import { cn } from '~/lib/utils';
 
 export function Spinner({ className }: { className?: ClassValue }) {
-  return <Loader2 size={50} className={cn('animate-spin', className)} />;
+  return <Loader2 size={50} className={cn('text-wedding-red animate-spin', className)} />;
 }
