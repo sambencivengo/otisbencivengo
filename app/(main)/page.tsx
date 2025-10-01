9f6f90fd90a336a0303130d19f778e39f7b5b1c7
Sam Bencivengo <sambencivengo@gmail.com>
2025-10-01 14:22:25 -0400

    fixed home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
 import Image from "next/image";
 import WeddingTitle from "../components/weddingTitle";
 
 export default function Home() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Check authentication status on client side
-    const auth = Cookies.get("isAuthenticated");
-    setIsAuthenticated(auth === "true");
-  }, []);
-
-  if (!isAuthenticated) {
-    return null; // Don't show anything if not authenticated
-  }
-
   return (
     <main className="flex flex-col">
       <WeddingTitle />
